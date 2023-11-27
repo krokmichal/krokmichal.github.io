f4e2dcfd3b3321083de17bf0f9f9c7a7d6d3e679
Michal Krok <krokmichal1@gmail.com>
2023-11-27 22:28:03 +0100

    deleting files

--- michal-krok/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-function clearForm() {
-    document.getElementById('name').value = "";
-    document.getElementById('email').value = "";
-    document.getElementById('phone').value = "";
-    document.getElementById('message').value = "";
-}

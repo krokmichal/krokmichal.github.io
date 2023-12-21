1ae8192a7adbc4659dd0b71bf33d2e155790e93a
Michal Krok <krokmichal1@gmail.com>
2023-12-21 19:12:03 +0100

    Webpage rebuild

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function clearForm() {
-    document.getElementById('name').value = "";
-    document.getElementById('email').value = "";
-    document.getElementById('phone').value = "";
-    document.getElementById('message').value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("message").value = "";
 }
